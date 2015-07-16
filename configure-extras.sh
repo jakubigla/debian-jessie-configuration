9bc42909efea1052978b8811e2921e4faff07c04
Jakub Igla <jakub.igla@valtech.co.uk>
2015-07-17 00:20:40 +0100

    docker compose

--- configure-extras.sh
+++ configure-extras.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install docker.io
+apt-get install docker.io
+curl -L https://github.com/docker/compose/releases/download/1.3.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 apt-get install apt-transport-https
 curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -
